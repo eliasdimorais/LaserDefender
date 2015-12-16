6f06e3a7b1a872ec82acc56dfb242b87c4a69c18
Elias de M. Fernandes <eliasdemoraes@live.com>
2015-12-16 02:25:22 -0200

    Commit Version 1.0 Assets to 2D game Laser Space

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform){
+			GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0), Quaternion.identity) as GameObject;
+			//put inside hierarchy 
+			enemy.transform.parent = transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Entities/EnemyFormation/Position.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	void OnDrawGizmos(){
+		Gizmos.DrawSphere(transform.position,1);
+	}
+}
