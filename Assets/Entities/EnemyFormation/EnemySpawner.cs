2cd50637beabf579ab43062993573a1a7c038dcb
Elias de M. Fernandes <eliasdemoraes@live.com>
2015-12-17 01:53:58 -0200

    Version 1.0 Update with player and enemies shooting

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -3,18 +3,48 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
+	public float width;
+	public float height;
+	public float speed = 5f;
 	
-	// Use this for initialization
+	
+	private bool movingRight = true;
+	private float xMax;
+	private float xMin;
+	// Camera.main.ViewportToWorldPoint -> It translates viewport coordinates to world coordinates so we can figure out where in the gamespace a point on the screen is.
+	
 	void Start () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoudary = Camera.main.ViewportToWorldPoint(new Vector3(0,0, distanceToCamera));
+		Vector3 rightBoudary = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distanceToCamera));
+		xMax = rightBoudary.x;
+		xMin = leftBoudary.x;
+		
 		foreach(Transform child in transform){
-			GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0), Quaternion.identity) as GameObject;
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			//put inside hierarchy 
-			enemy.transform.parent = transform;
+			enemy.transform.parent = child;
 		}
 	}
 	
+	public void OnDrawGizmos(){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		if(movingRight){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}else{
+			transform.position += new Vector3(-speed * Time.deltaTime,0);
+		}
+		
+		float rightEdgeOfFormation = transform.position.x + (0.5f*width);
+		float leftEdgeOfFormation = transform.position.x - (0.5f*width);
+		
+		if (leftEdgeOfFormation < xMin){
+			movingRight = true;
+		}else if(rightEdgeOfFormation > xMax){	
+			movingRight = false;		
+		}
 	}
 }
--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -2,22 +2,37 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	
+	public GameObject laser; 
+	public float projectileSpeed;
+	public float firingRate = 0.2f;
+	public float health;
 	public float speed = 15.0f;
 	public float padding;
+	
 	float xmin;
 	float xmax;
 	
 	void Start(){
-		
+		Camera camera = Camera.main;
 		float distance = transform.position.z - Camera.main.transform.position.z;
-		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
-		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
-		xmin = leftMost.x - padding;
-		xmax = rightMost.x - padding;
+		xmin = camera.ViewportToWorldPoint(new Vector3(0,0,distance)).x + padding;
+		xmax = camera.ViewportToWorldPoint(new Vector3(1,0,distance)).x - padding;
 		
 	}
-	
+	void Fire(){
+		Vector3 offset = new Vector3(0,1,0);
+		GameObject beam = Instantiate (laser, transform.position+offset, Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3(0, projectileSpeed,0);
+	}
 	void Update() {
+		//Dont use GetKey
+		if(Input.GetKeyDown(KeyCode.Space)){
+			InvokeRepeating("Fire", 0.00001f, firingRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke("Fire");
+		}
 		if(Input.GetKey(KeyCode.LeftArrow)){
 			//transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
 			transform.position += Vector3.left * speed * Time.deltaTime;
@@ -29,5 +44,18 @@ public class PlayerController : MonoBehaviour {
 		float newX = Mathf.Clamp(transform.position.x, xmin,xmax);
 		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			Debug.Log("Player Collided with a missile");
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+	
+	
 	
 }
